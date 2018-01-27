ab42a8f21af3e7899299aafdc6d4d72d19aa93c6
Chinegua <ai.chinegua@gmail.com>
2018-01-27 18:06:24 +0000

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { ANIMALES } from '../../data/data.animales' 
+import { Animal } from "../../interface/animal.interface";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales:Animal[] = [];
 
+  constructor(public navCtrl: NavController) {
+    this.animales = ANIMALES.slice(0); //COn slice hacemos un clon del objeto animales y no modificamos el original
+    console.log(this.animales);
+  }
+  reproducir(animal:Animal){
+    console.log(animal)
+
+    let audio = new Audio();
+    audio.src = animal.audio;
+    animal.reproduciendo = true;
+    audio.load();
+    audio.play();
+    setTimeout(()=> animal.reproduciendo = false, animal.duracion * 1000);
   }
 
 }
